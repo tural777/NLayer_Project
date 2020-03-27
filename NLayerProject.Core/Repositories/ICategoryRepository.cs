c1bb7d0c2434f4b464d90ffc243e42f28e497883
Tural Novruzov <TuraliNovruzov@outlook.com>
2020-03-27 05:25:23 +0400

    Add_IProductRepoAndICategoryRepo

---
+++ NLayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repositories
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+    }
+}
